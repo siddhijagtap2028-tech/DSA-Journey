0b7718b0c2fe899afca827f58d5b68b128ac0ae3
Siddhi <siddhijagtap2028@gmail.com>
2026-07-13 00:36:46 +0530

    Functions

---
+++ FUNCTIONS/factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int num){
+    int fact = 1;
+    for(int i=1;i<=num;i++){
+        fact=fact*i;
+    }
+    return fact;
+
+}
+
+int main(){
+    cout<<factorial(4);
+    return 0;
+}
