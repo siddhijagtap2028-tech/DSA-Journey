2278aac1b0160de5c40886c206792e37acc1ab75
Siddhi <siddhijagtap2028@gmail.com>
2026-07-16 00:38:09 +0530

    Basic

---
+++ Pointers/pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //dereference pointer
+    int a = 10;
+    //int *ptr = &a;
+   // cout<<*ptr;
+
+    //null pointer
+    int *ptr = NULL;
+    cout<<ptr<<endl;
+
+    cout<<*ptr;
+    return 0;
+}
