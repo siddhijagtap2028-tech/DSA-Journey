ea23c9cea159c6434c5f3b0b35df15101125c18e
Siddhi <siddhijagtap2028@gmail.com>
2026-07-25 23:56:17 +0530

    Array

--- ARRAY/BinarySearch.cpp
+++ ARRAY/BinarySearch.cpp
@@ -1,27 +1,29 @@
 #include<iostream>
 using namespace std;
 
+//only on monotonic func either inc or dec order 
 int BinarySearch(int arr[],int n,int key){
     int st = 0;
-    int end = n-1;
-    
-    while(st<= end ){
-        int mid = (st + end)/2;
-        if(arr[mid] == key){
+    int end = n;
+    while(st<=end){
+        int mid = (st+end) / 2;
+
+        if ( arr[mid] == key){
             return mid;
         }
         else if(arr[mid] > key){
-            end = mid-1;
+            return end = mid -1;
         }
         else{
-            st = mid + 1;
+            return st = mid+1;
         }
     }
+    
 }
 int main(){
-    int arr[6] = {2,4,6,7,9,10};
+    int arr[5] = {3,5,9,13,27};
     int n = sizeof(int)/sizeof(arr);
-    cout<<"key at index "<<BinarySearch(arr,6,9);
+    cout<<"key at index "<<BinarySearch(arr,5,9);
     
     return 0;
 }
