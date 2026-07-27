2d4ce77ab493e8add7172255afd9b44cd52d332d
Siddhi <siddhijagtap2028@gmail.com>
2026-07-27 23:09:43 +0530

    Array

--- ARRAY/occurence.cpp
+++ ARRAY/occurence.cpp
@@ -49,12 +49,13 @@ int lastOccurance(int arr[], int n, int key){
 
 int main(){
 
-    int arr[] = {1,4,5,5,6};
+    int arr[7] = {1,2,3,3,3,3,5};
 
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    cout << "First Occurrence = " << firstOccurance(arr, n, 5) << endl;
-    cout << "Last Occurrence = " << lastOccurance(arr, n, 5) << endl;
+    cout << "First Occurrence = " << firstOccurance(arr, n, 3) << endl;
+    cout << "Last Occurrence = " << lastOccurance(arr, n, 3) << endl;
+    
 
     return 0;
 }
