523dc1bd3e6b5df68011d2ac5c15b41c1889d108
Siddhi <siddhijagtap2028@gmail.com>
2026-07-28 23:51:23 +0530

    Array

--- ARRAY/MountainArray.cpp
+++ ARRAY/MountainArray.cpp
@@ -6,26 +6,28 @@ int mountainArray(int arr[],int n){
     int start = 0 ;
     int end = n -1;
     int mid = (start+end)/2;
-    while(start<=end){
+    while(start<end){
         if(arr[mid] < arr[mid+1]){
             start = mid + 1;
         }
         else{
-            return end;
+            end = mid;
         }
-    mid = (start+end)/2;
+        mid = (start+end)/2;
 
     }
+    return start;
+    
     
 }
 
 int main(){
 
-    int arr[5] = {0,1,2,4,3};
+    int arr[5] = {2,3,4,5};
 
-    int n = 5;
+    int n = 4;
 
-    cout << "First Occurrence = " << mountainArray(arr,5) << endl;
+    cout << "Peak Element at index = " << mountainArray(arr,n) << endl;
     
 
     return 0;
