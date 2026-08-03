fc00d79819813b2b4b135b9be13ee55e6fa2eab6
Siddhi <siddhijagtap2028@gmail.com>
2026-08-03 23:48:53 +0530

    Array

---
+++ ARRAY/simpleSearch.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int search(int nums[], int n, int target)
+{
+    int start = 0;
+    int end = n - 1;
+
+    while(start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if(nums[mid] == target)
+            return mid;
+
+        // Left half is sorted
+        if(nums[start] <= nums[mid])
+        {
+            if(target >= nums[start] && target < nums[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        // Right half is sorted
+        else
+        {
+            if(target > nums[mid] && target <= nums[end])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int nums[] = {4,5,6,7,0,1,2};
+    int n = 7;
+    int target = 0;
+
+    int ans = search(nums, n, target);
+
+    if(ans == -1)
+        cout << "Element not found";
+    else
+        cout << "Element found at index " << ans;
+
+    return 0;
+}
