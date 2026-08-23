677d6ae5d9be7105f8205e109d9b685ce4ea6e46
Siddhi <siddhijagtap2028@gmail.com>
2026-08-24 00:06:16 +0530

    Array

--- ARRAY/Aggresive_Cows.cpp
+++ ARRAY/Aggresive_Cows.cpp
@@ -1,29 +1,30 @@
 #include<iostream>
 #include<cmath>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 bool ispos(int arr[],int n,int k,int mid){
     int cowCount =1;
     int firstPos = arr[0];
 
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<n;i++){
         if(arr[i]-firstPos >= mid){
             cowCount++;
-            if(cowCount>k){
+            if(cowCount>=k){
                 return true;
             }
             firstPos=arr[i];
 
         }
     }
+    return false;
 }
 int aggreCows(int arr[],int n,int k){
-    sort(arr.begin(),arr.end());
+    sort(arr,arr+n);
     int s = 0;
-    int fistpos = arr[0];
     int maxi = -1;
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<n;i++){
         maxi=max(arr[i],maxi);
     }
     int end = maxi;
@@ -44,6 +45,6 @@ int main(){
     int arr[5] = {4,2,1,3,6};
     int n = 5;
     int k =2;
-    aggreCows(arr,n,k);
+    cout<<aggreCows(arr,n,k);
 
 }
